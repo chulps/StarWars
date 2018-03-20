b38aa9650e596c7a68d8c720bf3ddb4bdcbcb494
Chulps <chuckoward@gmail.com>
2018-03-20 11:06:48 -0700

    a;sdijf

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
